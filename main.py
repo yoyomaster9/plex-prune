dda82a9d62a2b5aa5d0cdbc4b9919ffd298ad091
yoyomaster9 <yoyomaster9@yahoo.com>
2024-05-20 22:52:13 -0700

    radarr deletion

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def get_movie_history(plex):
                 movie.title,
                 movie.addedAt.date(),
                 movie.locations[0],
+                os.path.dirname(movie.locations[0]), 
                 round(os.path.getsize(movie.locations[0])/ (1024 * 1024 * 1024), 3),
                 None,
                 None
@@ -33,6 +34,7 @@ def get_movie_history(plex):
                 movie.title,
                 movie.addedAt.date(),
                 movie.locations[0],
+                os.path.dirname(movie.locations[0]),
                 round(os.path.getsize(movie.locations[0])/ (1024 * 1024 * 1024), 3),
                 history.accountID,
                 history.viewedAt.date()
@@ -40,11 +42,11 @@ def get_movie_history(plex):
             )
 
 
-    movie_history_df = pd.DataFrame(d, columns = ['Title','AddedOn','Path', 'Size (GB)', 'AccountID', 'ViewedOn'])
+    movie_history_df = pd.DataFrame(d, columns = ['Title','AddedOn','Path', 'FolderPath', 'Size (GB)', 'AccountID', 'ViewedOn'])
     movie_history_df['AddedOn'] = pd.to_datetime(movie_history_df['AddedOn'])
     movie_history_df['ViewedOn'] = pd.to_datetime(movie_history_df['ViewedOn'])
     movie_history_df['AccountID'] = movie_history_df['AccountID'].astype('Int64')
-    movie_history_df = movie_history_df.groupby(['Title','AddedOn','Path', 'Size (GB)']).agg({'AccountID': 'count', 'ViewedOn':'max'}).reset_index()
+    movie_history_df = movie_history_df.groupby(['Title', 'AddedOn', 'Path', 'FolderPath', 'Size (GB)']).agg({'AccountID': 'count', 'ViewedOn':'max'}).reset_index()
     movie_history_df = movie_history_df.rename(columns={'AccountID': 'ViewCount', 'ViewedOn': 'LastViewedOn'})
     return movie_history_df
 
@@ -55,6 +57,14 @@ def filter_movie_history(movie_history_df):
     remove_movies_df = movie_history_df.query('LastViewedOn.isnull() & AddedOn < @d1 | LastViewedOn < @d2').reset_index(drop=True)
     return remove_movies_df
     
+def get_radarr_movies(RADARR_URL, RADARR_API_KEY):
+    headers = {'X-Api-Key': RADARR_API_KEY}
+    response = requests.get(f"{RADARR_URL}/api/v3/movie", headers=headers)
+    return pd.DataFrame(
+        {column: x[column] for column in ['id', 'title', 'monitored', 'sizeOnDisk',  'path', 'folderName']} for x in response.json()
+    )
+    
+
 def main():
     config = load_config()
 
@@ -78,7 +88,21 @@ def main():
     remove_movies_df = filter_movie_history(movie_history_df)
     remove_movies_df.to_csv('remove_movies_df.csv')
 
+    # Collect Radarr entries & join
+    radarr_movies_df = get_radarr_movies(RADARR_URL, RADARR_API_KEY)
+    remove_movies_df = remove_movies_df.merge(radarr_movies_df[['id', 'folderName']], left_on=['FolderPath'], right_on=['folderName']) \
+        .drop('folderName', axis=1) \
+        .rename(columns={'id': 'radarr_id'})
 
+    # Delete entries from Radarr
+    remove_movies_df['radarr_delete_response'] = \
+        remove_movies_df['radarr_id'].apply(
+            lambda x : requests.delete(
+                f"{RADARR_URL}/api/v3/movie/{x}?deleteFiles=true",
+                headers= {'X-Api-Key': RADARR_API_KEY}
+            ) \
+            .status_code
+        )
 
 if __name__ == '__main__':
     main()
